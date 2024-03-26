f05181a6c79d69c6d7a8709c2b91edaa93d49510
Akshay <akshaykhatri22@gmail.com>
2024-03-27 00:57:24 +0530

    Users Route Page Setup

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Users = () => {
-  return <div>Users</div>;
+  const [users, setUsers] = useState([
+    {
+      Name: "Akshay",
+      Email: "akshaykhatri22@gmail.com",
+      Age: "28",
+    },
+  ]);
+
+  return (
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      <div className="w-50 bg-white rounded p-3">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Age</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => {
+              return (
+                <tr>
+                  <td>{user.Name}</td>
+                  <td>{user.Email}</td>
+                  <td>{user.Age}</td>
+                  <td>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Users;
